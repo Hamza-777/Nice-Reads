2f3c866d490bb33ba6c32c51826d6288beb3a94e
Hamza Rarani <hamzararrani77@gmail.com>
2021-07-23 23:40:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const collection = {
+    prog: [
+      {
+        name: 'CRACKING the CODING INTERVIEW',
+        rating: '4.3/5'
+      },
+      {
+        name: 'Eloquent JavaScript',
+        rating: '4.1/5'
+      },
+      {
+        name: 'Head First Java',
+        rating: '4.2/5'
+      },
+      {
+        name: 'Grokking Algorithms',
+        rating: '4.4/5'
+      }
+    ],
+    fiction: [
+      {
+        name: 'The Lord of the Rings',
+        rating: '4.5/5'
+      },
+      {
+        name: 'To Kill a Mockingbird',
+        rating: '4.3/5'
+      },
+      {
+        name: 'Moby Dick',
+        rating: '3.5/5'
+      },
+      {
+        name: 'The Book Thief',
+        rating: '4.4/5'
+      }
+    ],
+    novel: [
+      {
+        name: 'One Hundred Years of Solitude',
+        rating: '4.1/5'
+      },
+      {
+        name: 'The Great Gatsby',
+        rating: '3.9/5'
+      }
+    ]
+  };
+
+  const [which, setWhich] = useState('prog');
+
+  const handleClick = (e) => {
+    setWhich(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <nav>
+          <h1>📚 Nice Reads</h1>
+        </nav>
       </header>
+      <div className='container'>
+        <p>Checkout my collection of favourite books 📕📗📙. Pick a genre to get started.</p>
+        <div className='buttons'>
+          <button onClick={handleClick} value='prog'>Programming</button>
+          <button onClick={handleClick} value='fiction'>Fiction</button>
+          <button onClick={handleClick} value='novel'>Novel</button>
+        </div>
+        <div className='books'>
+          {
+            collection[which].map((item) => {
+            return <div key={item.name} className="box"><h3>{item.name}</h3><p>{item.rating}</p></div>;
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
